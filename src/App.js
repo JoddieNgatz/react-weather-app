0b43c79dba1a33fd61bfcaa8be48d49d6ecbb05d
Joseph Mureithi <ngatia.joseph@gmail.com>
2023-02-12 06:38:01 +0300

    weather js with co-ordinates and get weather with fetch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-
+import './weather/weather'
+import Weather from './weather/weather';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      
+      <Weather/>
       </header>
     </div>
   );
