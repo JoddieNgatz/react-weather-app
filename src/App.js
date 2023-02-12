f498bfe38427030488c41638636df0ccc2be0beb
Joseph Mureithi <ngatia.joseph@gmail.com>
2023-02-12 19:49:10 +0300

    weather component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
 import './App.css';
 import './weather/weather'
-import Weather from './weather/weather';
+//import Weather from './weather/weather';
+import CardComponent from './components/card';
+import React, { useEffect, useState } from "react";
+
+
 function App() {
+const [lat, setLat] = useState([]);
+const [long, setLong] = useState([]);
+const [weatherData, setData]=useState([]);
+useEffect(()=>{
+    const fetchData = async () =>{
+        
+        navigator.geolocation.getCurrentPosition(function(position) {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      });
+  console.log(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`);
+  await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+  .then(res => res.json())
+      .then(result => {
+        console.log(result);
+        setData(result);
+      }).catch((e)=>{
+        console.log(e);
+      });
+    }
+    fetchData()
+    }, [lat, long]);
+
   return (
     <div className="App">
       <header className="App-header">
-      <Weather/>
+      {(typeof weatherData.main != 'undefined') ? (
+        <CardComponent weatherData={weatherData}/>
+      ): (
+        <div>
+          <p>Loading....</p>
+        </div>
+      )}
       </header>
     </div>
   );
--- src/weather/weather.js
+++ src/weather/weather.js
@@ -1,35 +1,35 @@
-import './App.css';
-import React, { useEffect, useState } from "react";
+import './weather.css';
+//import React, { useEffect, useState } from "react";
 
 
-function Weather() {
-const [lat, setLat] = useState([]);
-const [long, setLong] = useState([]);
-const [weatherData, setData]=useState([]);
-useEffect(()=>{
-    const fetchData = async () =>{
+function Weather({weatherData}) {
+// const [lat, setLat] = useState([]);
+// const [long, setLong] = useState([]);
+// const [weatherData, setData]=useState([]);
+// useEffect(()=>{
+//     const fetchData = async () =>{
         
-        navigator.geolocation.getCurrentPosition(function(position) {
-        setLat(position.coords.latitude);
-        setLong(position.coords.longitude);
-      });
+//         navigator.geolocation.getCurrentPosition(function(position) {
+//         setLat(position.coords.latitude);
+//         setLong(position.coords.longitude);
+//       });
   
-      console.log("Latitude is:", lat)
-      console.log("Longitude is:", long)
-      await fetch(`${process.env.API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.API_KEY}`)
-      .then(res => res.json())
-      .then(result => {
-        setData(result)
-        console.log(result);
-      });
-    }
-    fetchData()
-    }, [lat, long]);
+//       console.log("Latitude is:", lat)
+//       console.log("Longitude is:", long)
+//       await fetch(`${process.env.API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.API_KEY}`)
+//       .then(res => res.json())
+//       .then(result => {
+//         setData(result)
+//         console.log(result);
+//       });
+//     }
+//     fetchData()
+//     }, [lat, long]);
 
   return (
     <div className="Weather">
       <header className="Weather-header">
-
+<card weatherData={weatherData}/>
       </header>
     </div>
   );
